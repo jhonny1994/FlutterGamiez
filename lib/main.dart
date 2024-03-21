188682bac4acf49caece5988b04e81582ab24a5a
mohamed abderraouf douadi <me.is.intik@gmail.com>
2024-03-21 13:13:24 +0100

    added theme added logo added needed packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gamiez/theme/app_theme.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
     );
   }
 }
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme(
+    background: Color(0xff1c1e27),
+    brightness: Brightness.dark,
+    primary: Colors.white,
+    onPrimary: Color(0xff1c1e27),
+    secondary: Color(0xfff74455),
+    onSecondary: Colors.white,
+    error: Colors.red,
+    onError: Colors.white,
+    onBackground: Colors.white,
+    surface: Color(0xff1c1e27),
+    onSurface: Colors.white,
+  ),
+);
